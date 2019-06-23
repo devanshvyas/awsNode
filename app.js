b92c213cfe576f67b9aa2e1ec44fd353f6ada276
devanshvyas <40229236+devanshvyas@users.noreply.github.com>
2019-06-23 18:54:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Session #2 - Deep Dive into AWS EC2');
+  res.end('AWS NODE 2019');
 }).listen(8000);
