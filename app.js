8571817546d22e536298b2cc05cb1f74659b3246
devanshvyas <40229236+devanshvyas@users.noreply.github.com>
2019-06-23 19:15:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 
+var html = fs.readFileSync('hello-world.html');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('AWS NODE 2019');
+  res.end(html);
 }).listen(8000);
